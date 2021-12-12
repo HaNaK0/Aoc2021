f520ec9f6dcf109a9bf6c02b345d30888cf9bc87
HaNaK0 <hampus@hampushuledal.com>
2021-12-12 16:59:55 +0100

    day_1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
+use std::{fs, io::{Error, BufReader, BufRead}};
+
+fn main() -> Result<(), Error> {
     println!("Hello, world!");
+
+    day_1()?;
+
+    Ok(())
+}
+
+fn day_1() -> Result<(), Error> {
+    println!("Day 1");
+
+    let file = fs::File::open("data/day_1.txt")?;
+    let file = BufReader::new(file);
+    
+    let lines = file.lines();
+
+    let line_vector : Vec<i32> = lines.map(|item| -> i32 {
+        let number = item.unwrap().parse::<i32>().unwrap();
+        number
+    }).collect();
+
+    let result = line_vector.iter()
+        .enumerate()
+        .map_while(|item| {
+            if item.0 + 2 >= line_vector.len() {
+                None
+            } else {
+                let mut sum = *item.1;
+                sum += line_vector[item.0 + 1];
+                sum += line_vector[item.0 + 2];
+                Some((sum, 0))
+            }
+    }).reduce(|accum, item| {
+        if accum.0 < item.0 {
+            (item.0, accum.1 + 1)
+        } else {
+            (item.0, accum.1)
+        }
+    }).unwrap();
+
+    println!("Result: {}", result.1);
+
+    Ok(())
 }
